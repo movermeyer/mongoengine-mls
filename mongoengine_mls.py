2675250f76fb1e13d83ad6a8ebef77eefc30c9e1
Aleksey Rembish <aleksey.rembish@firma.seznam.cz>
2015-04-17 16:58:38 +0200

    Documentation

--- mongoengine_mls.py
+++ mongoengine_mls.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+# pylint: disable=W0212,C0103
+"""
+Unicode-like field for multiple language mutations, which can be used with
+MongoEngine package.
+"""
 from copy import deepcopy
 from mongoengine.document import EmbeddedDocument
 from mongoengine.fields import ListField, EmbeddedDocumentField, StringField
 from mls import mls
 from sys import version_info
+
 __all__ = ["MultiLingualField"]
 
 if version_info < (3, 0, 0):
@@ -13,15 +19,32 @@ else:
 
 
 class MultiLingualEmbeddedDocument(EmbeddedDocument):
+    """
+    Internal embedded document to store information about language mutations.
+    You can simply apply index on any field in your Document, which uses
+    MultiLingualField. For example:
+
+        >>> from mongoengine import Document
+        >>> from mongoengine_mls import MultiLingualField
+        >>> class Country(Document):
+        ...     meta = {"indexes": ["name.language"]}
+        ...     name = MultiLingualField()
+    """
     language = StringField(required=True, min_length=2, max_length=3)
     value = StringField(required=True)
 
 
 class MultiLingualField(ListField):
+    """
+    Special field, that will be automatically transformed to MultiLingualString
+    after extracting from MongoDB. See `mls` package documentation for more
+    advantages and useful tips.
+    """
     def __init__(self, **kwargs):
         null = kwargs.pop("null", False)
         super(MultiLingualField, self).__init__(
-            field=EmbeddedDocumentField(MultiLingualEmbeddedDocument), **kwargs)
+            field=EmbeddedDocumentField(MultiLingualEmbeddedDocument), **kwargs
+        )
         self.null = null
 
     def __set__(self, instance, value):
@@ -38,6 +61,17 @@ class MultiLingualField(ListField):
                     or instance._data[self.name] != value \
                     or type(instance._data[self.name]) != type(value):
                 instance._mark_as_changed(self.name)
+
+        value = deepcopy(value)
+        if isinstance(value, (dict, string_types)):
+            value = mls(value)
+        elif isinstance(value, (list, tuple, set, frozenset)):
+            try:
+                value = mls(**dict(
+                    (item["language"], item["value"]) for item in value
+                ))
+            except (KeyError, TypeError, ValueError):
+                pass
 
         instance._data[self.name] = value
 
